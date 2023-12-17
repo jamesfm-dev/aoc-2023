f73502a10f239ccc489f88cf0b58a9f0c71f82e6
James Francis Malquerido <66410095+jamesfm-dev@users.noreply.github.com>
2023-12-17 07:23:32 +0000

    add day 2, part 1 golang implementation

---
+++ day02/go/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	debug := len(os.Args) == 3 && (os.Args[2] == "-d" || os.Args[2] == "--debug")
+
+	if len(os.Args) < 2 {
+		log.Fatal("error no input provided\n")
+	}
+
+	filename := os.Args[1]
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("error failed to open file %s: %v\n", filename, err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	answer := 0
+	for scanner.Scan() {
+		game := strings.Fields(scanner.Text())
+		games := game[2:]
+		gameNumber := strings.Split(game[1], ":")[0]
+		colorSet := map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+		sets := make([]map[string]int, len(games)/2)
+		setCounter := 1
+		valid := true
+
+		for i := 0; i < len(sets); i = i + 1 {
+			color := games[2*i+1]
+			valueStr := games[2*i]
+
+			color = strings.TrimRightFunc(color, func(r rune) bool {
+				return r == ';' || r == ','
+			})
+
+			value, err := strconv.Atoi(valueStr)
+			if err != nil {
+				log.Fatalf("failed to convert value %s: %v\n", valueStr, err)
+			}
+
+			if (color == "red" && value > 12) ||
+				(color == "green" && value > 13) ||
+				(color == "blue" && value > 14) {
+				valid = false
+			}
+
+			colorSet[color] = value
+			sets[setCounter-1] = colorSet
+			if strings.ContainsRune(games[2*i+1], ';') {
+				clear(colorSet)
+				setCounter++
+			}
+		}
+
+		if debug {
+			fmt.Printf("[Game %s] = %v\n", gameNumber, games)
+			fmt.Printf("[Sets %d] = %v\n\n", setCounter, sets[:setCounter])
+		}
+
+		if valid {
+			x, err := strconv.Atoi(gameNumber)
+			if err != nil {
+				log.Fatalf("error failed to convert gameNum to int %s: %v", gameNumber, err)
+			}
+			answer += x
+		}
+	}
+
+	if debug {
+		fmt.Printf("Answer: %d\n", answer)
+		return
+	}
+	fmt.Print(answer)
+}
